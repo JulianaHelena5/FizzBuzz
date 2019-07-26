30c6543f60dc9d74b1bc4c3284572c1b9f8376bc
JulianaHelena <juliana.helena15@gmail.com>
2019-07-26 15:14:50 -0300

    Refatorando

--- app/fizzbuzz.rb
+++ app/fizzbuzz.rb
@@ -5,9 +5,17 @@
 class FizzBuzz
   def convert(number)
     converted_number = ''
-    converted_number += 'fizz' if (number % 3).zero?
-    converted_number += 'buzz' if (number % 5).zero?
+    converted_number += 'fizz' if multiple_of_3?(number)
+    converted_number += 'buzz' if multiple_of_5?(number)
     converted_number += number.to_s if converted_number == ''
     converted_number
+  end
+
+  def multiple_of_3?(number)
+    (number % 3).zero?
+  end
+
+  def multiple_of_5?(number)
+    (number % 5).zero?
   end
 end
